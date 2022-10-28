57ffcb9f6ba21bc7ec4484b95480d78c490dc609
MrPolloo <70990197+MrPolloo@users.noreply.github.com>
2022-10-28 15:17:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./routes');
-const port = process.env.PORT
+const port = process.env.PORT || 2000
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, ()=>{
-    console.log("app runing di port 2000");
+    console.log("app runing di port http://localhost:"+ port);
 });
