4a71f4acbf44357ea86bed82c97ed015010e41b2
MrPolloo <70990197+MrPolloo@users.noreply.github.com>
2022-10-28 15:01:47 +0700

    Deployment

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./routes');
-
+const port = process.env.PORT
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(2000, ()=>{
+app.listen(port, ()=>{
     console.log("app runing di port 2000");
 });
