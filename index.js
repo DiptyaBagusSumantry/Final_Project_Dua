9aa7d6cbcc5e65cdc08c3ca17c9c67d306c2775b
Diptya Bagus Sumantry <112381720+DiptyaBagusSumantry@users.noreply.github.com>
2022-10-29 11:47:56 +0700

    back coba register

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./routes');
-const UserController = require('./controllers/UserController');
 const port = process.env.PORT || 2000
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-app.post('/users/register', UserController.register);
 
 app.use(routes);
 
